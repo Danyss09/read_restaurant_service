2cf4470f0572331e768ade3a290ee110a800168f
Danyss09 <danielacaceresp4@gmail.com>
2025-02-07 17:20:16 -0500

    Update db_config.js

--- services/db_config.js
+++ services/db_config.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
   try {
     console.log('🌍 MONGO_URI:', process.env.MONGO_URI);  // ✅ Depuración
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log('✅ Connected to MongoDB:', mongoose.connection.name);
   } catch (error) {
