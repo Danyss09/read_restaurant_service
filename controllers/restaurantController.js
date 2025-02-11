a3950a4f90bb7ac9da0340671f37bfbcd0b6cddb
Danyss09 <112229585+Danyss09@users.noreply.github.com>
2025-02-11 16:32:29 -0500

    Update restaurantController.js

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -7,7 +7,7 @@ exports.getRestaurantById = async (req, res) => {
     const restaurant = await Restaurant.findById(id);
 
     if (!restaurant) {
-      return res.status(404).json({ error: 'Restaurant not found' });
+      return res.status(404).json({ error: 'Restaurant not found :c' });
     }
 
     res.status(200).json(restaurant);
